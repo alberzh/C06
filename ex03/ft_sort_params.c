0f2b780d6da9114be2bc9e49722e2cc3672030fa
Alberto Zaragoza <alberzh@gmail.com>
2025-07-31 12:12:25 +0200

    Ejercicio ex03 Listo.

--- ex03/ft_sort_params.c
+++ ex03/ft_sort_params.c
@@ -6,31 +6,71 @@
 /*   By: azaragoz <azaragoz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 17:24:24 by azaragoz          #+#    #+#             */
-/*   Updated: 2025/07/30 17:41:15 by azaragoz         ###   ########.fr       */
+/*   Updated: 2025/07/31 12:10:10 by azaragoz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// Los argumentos a ordenar son desde argv[1] hasta argv[argc - 1].
-// La comparación para ordenar se hace con la función strcmp (o una comparación manual que respete el orden ASCII).
-
 #include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char	*swap;
+
+	swap = *a;
+	*a = *b;
+	*b = swap;
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
 
 int	main(int argc, char **argv)
 {
 	int	i;
 	int	j;
 
-	i = 1;
-	while (i < argc)
+	if (argc > 1)
 	{
-		j = 0;
-		while (argv[i][j])
+		i = 1;
+		while (i < argc - 1)
 		{
-			write(1, &argv[i][j], 1);
+			if (ft_strcmp(argv[i], argv[i + 1]) > 0)
+			{
+				ft_swap(&argv[i], &argv[i + 1]);
+				i = 1;
+			}
+			else
+				i++;
+		}
+		j = 1;
+		while (j < argc)
+		{
+			ft_putstr(argv[j]);
+			write(1, "\n", 1);
 			j++;
 		}
-		write(1, "\n", 1);
-		i++;
 	}
 	return (0);
 }
